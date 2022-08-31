f8cbd255a9064968c8775ce8532fb7c0adb09350
suraj chapagain <surajchapagain7743@gmail.com>
2022-08-31 10:42:27 +0545

    storing data from body-parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 require("./utils/db.config");
+app.use(bodyParser.urlencoded({ extended: false }));
+const userModel = require("./models/userModel");
 
 app.set("view-engine", "ejs");
 app.get("/", (req, res) => {
@@ -9,8 +12,11 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
   return res.render("register.ejs");
 });
-app.post("/register", (req, res) => {
-  return res.send("Form submitted ");
+app.post("/register", async (req, res) => {
+  const user = new userModel(req.body);
+  await user.save();
+
+  return res.send(user);
 });
 app.listen(8000, () => {
   console.log("listening to server 8000 ");
--- models/userModel.js
+++ models/userModel.js
@@ -1,29 +1,28 @@
 const mongoose = require("mongoose");
 const userSchmea = mongoose.Schema({
   name: {
-    typeof: String,
+    type: String,
     required: [true, "must include a name "],
     maxlength: [60, "Name can't be greater than 60 character "],
     minlength: [2, "Minlength should be of 2 character long "],
   },
   email: {
-    typeof: String,
+    type: String,
     required: [true, "Email is required"],
     maxlength: [128, "Email can't be greater than 120 characters"],
     index: true,
   },
   password: {
-    typeof: String,
+    type: String,
     required: [true, "Must include a password"],
   },
   isActive: {
-    typeof: Boolean,
+    type: Boolean,
     default: true,
   },
   idDeleted: {
-    typeof: Boolean,
+    type: Boolean,
     default: false,
   },
-  timestamps: true,
 });
 module.exports = mongoose.model("n-ecommerceUser", userSchmea);
